0f2a94a68b0a31a157a501c0a343270075104ba0
Bradford-Meziere <coleokc@gmail.com>
2021-09-22 14:33:09 -0500

    HerokuPush again

--- knexfile.js
+++ knexfile.js
@@ -8,26 +8,23 @@ if (process.env.DATABASE_URL) {
 
 const sharedConfig = {
   client: 'pg',
-  migrations: { directory: './data/migrations'},
-  seeds: { directory: './data/seeds' },
+  migrations: { directory: './app/data/migrations'},
+  seeds: { directory: './app/data/seeds' },
 }
 
 module.exports = {
   development: {
     ...sharedConfig,
     connection: process.env.DEV_DATABASE_URL,
-    pool: { min: 2, max: 10 },
   },
   testing: {
     ...sharedConfig,
     connection: process.env.TESTING_DATABASE_URL,
   },
   production: {
+    ...sharedConfig,
     client: 'pg',
     connection: process.env.DATABASE_URL,
-    migrations: {
-        directory: './data/migrations',
-    },
-    seeds: { directory: './data/seeds' },
+    pool: { min: 2, max: 10 },
 }
 }
